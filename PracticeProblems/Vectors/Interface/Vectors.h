05e90af7d1a10188f72a174bf5f8078df1a01802
Nishant <nishantpatra16@gmail.com>
2025-03-27 14:46:26 +0000

    cmake issue fixed

---
+++ PracticeProblems/Vectors/Interface/Vectors.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void getFirstElementOfVector(const vector<string>& myVector);
+void lastElementOfVector(const vector<string>& myVector);
+void swapFirstAndLastElementsOfVectors(vector<string>& myVector);
+void getParticularIndexedValue(vector<string>& myVector, int index);
